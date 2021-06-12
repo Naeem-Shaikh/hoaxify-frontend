e18df4452439a9828d3ab3eaa876aece9b21f17a
naeemshaikh <naeem5@yahoo.com>
2021-06-11 23:16:23 -0400

    User Sign up page added. Closes #1

---
+++ src/pages/UserSignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class UserSignUpPage extends React.Component{
+
+    render(){
+        return(<div>
+            <h1>Sign Up</h1>
+        </div>)
+    }
+}
+
+export default UserSignUpPage
